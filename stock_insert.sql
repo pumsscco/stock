7094694167d51adf6f02154671226cd3dc2b8d27
Pluto Chan <pumsscco@163.com>
2022-08-10 09:44:23 +0800

    08-10前交易记录入库

--- stock_insert.sql
+++ stock_insert.sql
@@ -1,5 +1,5 @@
 insert into `stock`
 (`date`,`code`,`name`,`operation`,`volume`,`balance`,`price`,`turnover`,`amount`,`brokerage`,`stamps`,`transfer_fee`)
 values 
-('2022-07-25', '600686', '金龙汽车', '证券买入', 6500, 28200, 7.71, 50115, -50128.03, 12.53, 0, 0.5);
+('2022-08-04', '600649', '城投控股', '股息入账', 0, 40900, 3.97, 4499, 4499, 0, 0, 0);
 
