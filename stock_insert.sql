09ccc5282881a4509091c0136de74090d4a79cf1
Pluto Chan <pumsscco@163.com>
2023-06-09 23:47:33 +0800

    增加5月中至6月初的建工操作记录，这次趁机抛弃，减持获利，比较成功！

--- stock_insert.sql
+++ stock_insert.sql
@@ -1,4 +1,4 @@
 insert into `stock`
 (`date`,`code`,`name`,`operation`,`volume`,`balance`,`price`,`turnover`,`amount`,`brokerage`,`stamps`,`transfer_fee`)
 values 
-('2023-05-15', '601789', '宁波建工', '证券买入', 8400, 85100, 4.77, 40068, －40078.42, 10.02, 0, 0.4);
+('2023-06-08', '601789', '宁波建工', '证券卖出', -3900, 81200, 5.13, 20007, 19981.79, 5, 20.01, 0.2);
