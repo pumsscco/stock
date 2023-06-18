5ae7690d0fa5502c624a9833ef6b2186eaab20ec
Pluto Chan <pumsscco@163.com>
2023-06-18 20:44:54 +0800

    宁波联合股息入账

--- stock_insert.sql
+++ stock_insert.sql
@@ -1,4 +1,4 @@
 insert into `stock`
 (`date`,`code`,`name`,`operation`,`volume`,`balance`,`price`,`turnover`,`amount`,`brokerage`,`stamps`,`transfer_fee`)
 values 
-('2023-06-08', '601789', '宁波建工', '证券卖出', -3900, 81200, 5.13, 20007, 19981.79, 5, 20.01, 0.2);
+('2023-06-15', '600051', 'XD宁波联合', '股息入账', 0, 48800, 7.38, 5856, 5856, 0, 0, 0);
