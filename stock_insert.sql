1d97bfd61dece1593b707c534d06061cca2dfc46
Pluto Chan <pumsscco@163.com>
2023-07-30 11:37:54 +0800

    城投控股卖了些，记录一下

--- stock_insert.sql
+++ stock_insert.sql
@@ -1,4 +1,4 @@
 insert into `stock`
 (`date`,`code`,`name`,`operation`,`volume`,`balance`,`price`,`turnover`,`amount`,`brokerage`,`stamps`,`transfer_fee`)
 values 
-('2023-06-15', '600051', 'XD宁波联合', '股息入账', 0, 48800, 7.38, 5856, 5856, 0, 0, 0);
+('2023-07-28', '600649', '城投控股', '证券卖出', -4500, 44500, 4.44, 19980, 19954.84, 5, 19.98, 0.18);
